7f0c62642664664d01dc9c6cc00c3deba1246a80
rwqwqrwq-cloud <rwqwqrwq@gmail.com>
2025-08-11 00:43:57 +0700

    Update server.js

--- server.js
+++ server.js
@@ -70,8 +70,9 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+
