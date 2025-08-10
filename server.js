fd1feaa102a0302e7496f57b089cafbebdf14cbf
rwqwqrwq-cloud <rwqwqrwq@gmail.com>
2025-08-11 00:41:28 +0700

    Update server.js

--- server.js
+++ server.js
@@ -60,4 +60,18 @@ app.post("/send-order", upload.single("file"), async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
+
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
